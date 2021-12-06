21b2e6ba26c8e1263fff502fe06783fe508db4c6
Kevin-Souza <78280970+Kevin-Souza@users.noreply.github.com>
2021-12-06 20:40:23 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaWeb/Controllers/FornecedoresController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProvaWeb.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        // GET: Fornecedores
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProvaWeb/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProvaWeb.Models
+{
+    public class Produto
+    {
+        public long Produtoid { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
