36ea56e467bfe852bc1f1942020eca0909922542
Kevin-Souza <78280970+Kevin-Souza@users.noreply.github.com>
2021-12-06 21:19:42 -0300

    Create views Produtos

---
+++ ProvaWeb/Context/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using ProvaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProvaWeb.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("PROVA") { }
+        public DbSet<Fornecedor> fornecedores { get; set; }
+        public DbSet<Produto> produtos { get; set; }
+    }
+}
--- ProvaWeb/Controllers/ProdutosController.cs
+++ ProvaWeb/Controllers/ProdutosController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ProvaWeb.Context;
+using ProvaWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,88 @@ namespace ProvaWeb.Controllers
 {
     public class ProdutosController : Controller
     {
+        private EFContext context = new EFContext();
+
         // GET: Produtos
         public ActionResult Index()
         {
+            return View(context.produtos.OrderBy(p => p.Nome));
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Produto produto)
+        {
+            context.produtos.Add(produto);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(produto).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto);
+        }
+
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Produto produto = context.produtos.Find(id);
+            context.produtos.Remove(produto);
+            context.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
